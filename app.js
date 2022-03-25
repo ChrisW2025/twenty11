ce7b1b46da94696623147bc184b057fccc04fade
Whitaker <cwhitaker@FVFZMY1CJ1WK.tld>
2022-03-25 10:55:11 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+function askName() {
+    var name = prompt("What is your name?")
+return askName
+}
+askName()
+
+function random() {
+    var ranNum =  Math.floor(Math.random() * 11)
+    alert('')
+    return random
+}
+random()
