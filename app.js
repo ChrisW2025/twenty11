fcd92cb9ce205da2622ce3ec7384abae73cc6992
Whitaker <cwhitaker@FVFZMY1CJ1WK.tld>
2022-03-29 10:57:54 -0500

    added the user and computer functions

--- app.js
+++ app.js
@@ -1,12 +1,51 @@
+
+
 function askName() {
     var name = prompt("What is your name?")
 return askName
 }
-askName()
+// askName()   
 
 function random() {
-    var ranNum =  Math.floor(Math.random() * 11)
-    alert('')
+    var random = Math.floor(Math.random() * 10) + 1
     return random
 }
-random()
+
+function user() {
+
+  var me = 0
+
+  do {
+
+    var me = me + random()
+
+    if (me > 20) {
+      break
+    }
+
+    var useran = prompt(`Your new number is ${me}. Press "N to stop"`)
+
+  }while ((me <=21) && (useran !="N"))
+
+  return me
+}
+alert(user())
+
+function computer() {
+var comp = 0
+
+do {
+
+    var comp = comp + random()
+
+    alert(`The computers new number is ${comp}.`)
+
+}while (comp <= 16);
+
+return comp
+}       
+alert(computer())
+
+function compare() {
+  
+}
