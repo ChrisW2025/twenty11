393e55158ff6aa591936cec21e39efb31a89ba83
Whitaker <cwhitaker@FVFZMY1CJ1WK.tld>
2022-03-31 10:35:27 -0500

    finsihed

--- app.js
+++ app.js
@@ -4,8 +4,7 @@ function askName() {
     var name = prompt("What is your name?")
 return askName
 }
-// askName()   
-
+ 
 function random() {
     var random = Math.floor(Math.random() * 10) + 1
     return random
@@ -29,7 +28,7 @@ function user() {
 
   return me
 }
-alert(user())
+
 
 function computer() {
 var comp = 0
@@ -44,8 +43,53 @@ do {
 
 return comp
 }       
-alert(computer())
+
 
 function compare() {
-  
+  var me = user()
+  var comp = computer()
+
+  if  ((comp > 20) && (me > 20))  {
+    alert("Looks like both you and the computer have lost.")
+    counterlosep-push(i)
+    counterlosec-push(i)
+  }
+    else if (comp > 20) {
+    alert(`Congratualtions, you have won with a score of ${me}`)
+    counterwinp. push (i)
+    counterlosec.push(i)
+    }
+    else if (me > 20) {
+    alert( `Sorry you lost, the computer won with a score of ${comp}`)
+    counterinc.push(i)
+    counterlosep-push(i)
+    }
+    else if ((me <= 20) && (comp <= 20)) {
+      if (me > comp) {
+      alert( `Congratualtions, you have won with a score of ${me}}`)
+      counterwinp. push (i)
+      counterlosec,push(i)
+      }
+      else if (me < comp) {
+      alert(`Sorry you lost, the computer won with a score of ${comp}`)
+      counterwinc,push(i)
+      counterlosen, push(1)
+      }
+      else if (me = comp) {x
+      alert(`You have both tied with a score of ${me}`)
+      }
+    }
+  }
+
+function master() {
+  compare()
+  var q = prompt("If you want to play again, press 1. Otherwise, thank you.")
+  if (q == "1") {
+    compare()
+  }
 }
+
+askName()
+master()  
+
+
